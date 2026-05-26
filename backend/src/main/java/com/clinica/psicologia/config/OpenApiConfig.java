0fd7566366ca4141a7fe917b0110e8b7e5fdeb19
dantequiP <danteqp.10@gmail.com>
2026-05-26 14:57:24 -0500

    refactor: mover emision de tickets a anfitriona(back)

---
+++ backend/src/main/java/com/clinica/psicologia/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.clinica.psicologia.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "API Clínica Psicología", version = "1.0"), security = {
+        @SecurityRequirement(name = "bearerAuth")
+})
+@SecurityScheme(name = "bearerAuth", description = "JWT Authorization header usando Bearer token", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfig {
+}
